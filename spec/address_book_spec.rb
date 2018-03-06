dd86c5d27e2e89131ac05cb2ea058e2427ddf6d1
CassadyBlake <cassady.lillstrom@gmail.com>
2018-03-05 16:01:10 -0800

    addressBloc models assignment: completed

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -37,4 +37,17 @@ RSpec.describe AddressBook do
     end
   end
 
+  describe "#remove_entry" do
+    it "removes only one entry of the address book" do
+      book = AddressBook.new
+      book.add_entry('Tim Simons', '303.498.2332', 'tim.simons@baseline.com')
+      book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+      entries = book.entries
+
+      expect(entries.size).to eq(2)
+      book.remove_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+      expect(entries.size).to eq(1)
+      expect(entries[0].name).to eq('Tim Simons')
+    end
+  end
 end
